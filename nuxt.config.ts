59ca14860444615f5421ec8b94f82827d41b54c6
Andrei Ujica <andrei@vipra.tech>
2024-07-16 03:27:00 +0300

    feat(client): add header component

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@pinia/nuxt"]
+  modules: ["@nuxt/ui", "@pinia/nuxt", "@nuxt/fonts"],
+  ui: {
+    icons: ['tabler'],
+  },
+  css: ['~/assets/css/main.css']
 })
